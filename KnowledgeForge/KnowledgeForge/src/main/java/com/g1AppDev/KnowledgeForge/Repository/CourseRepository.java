713e9bd4c25fb18b9d2cb96252d101cf209aae39
James Wolfe <jamesernest.wolfe@cit.edu>
2024-10-24 01:00:50 +0800

    UPDATE NANI HA, MAAM BARBS

--- KnowledgeForge/KnowledgeForge/src/main/java/com/g1AppDev/KnowledgeForge/Repository/CourseRepository.java
+++ KnowledgeForge/KnowledgeForge/src/main/java/com/g1AppDev/KnowledgeForge/Repository/CourseRepository.java
@@ -1,8 +1,10 @@
 package com.g1AppDev.KnowledgeForge.Repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import com.g1AppDev.KnowledgeForge.Entity.Course;
 
+@Repository
 public interface CourseRepository extends CrudRepository<Course, Integer> {
     
 }
